ddb7a24e8a5074a34e4d1967979510f1bbf5a12d
Youka <spanknebel.borken@t-online.de>
2013-03-23 12:34:18 +0100

    added logo example; added Lua 5.2 documentation

---
+++ examples/video/logo.lua
@@ -0,0 +1,41 @@
+-- Create logo image
+local logo_image = g2d.create_image("RGB", 150,150)
+do	-- (separate block to garbage-collect drawing objects)
+	-- Create logo image context
+	local logo_ctx = g2d.create_context(logo_image)
+	-- Create radial gradients
+	local red_arc = g2d.create_source_radial_gradient(20,65,0, 20,65,40):add_color(0,1,0,0,1):add_color(1,1,0,0,0)
+	local green_arc = g2d.create_source_radial_gradient(70,90,0, 70,90,40):add_color(0,0,1,0,1):add_color(1,0,1,0,0)
+	local blue_arc = g2d.create_source_radial_gradient(130,60,0, 130,60,40):add_color(0,0,0,1,1):add_color(1,0,0,1,0)
+	-- Create motion blur kernel
+	local blur_kernel = g2du.create_motion_blur_kernel(8,45)
+	-- Define text path
+	logo_ctx:set_matrix(g2d.create_matrix():translate(6,23))
+	logo_ctx:path_add_text("LVS", "Comic Sans MS", 100, true, false, false, false)
+	logo_ctx:set_matrix(g2du.identity)
+	-- Set composition
+	logo_ctx:set_composition("ADD")
+	-- Draw 3 times
+	for i=1, 3 do
+		-- Draw gradients in path
+		logo_ctx:set_source(red_arc)
+		logo_ctx:path_fill()
+		logo_ctx:set_source(green_arc)
+		logo_ctx:path_fill()
+		logo_ctx:set_source(blue_arc)
+		logo_ctx:path_fill()
+		-- Apply motion blur on first 2 passes
+		if i<3 then
+			g2d.image_convolution(logo_image, blur_kernel)
+		end
+	end
+end
+
+-- Process frames
+function GetFrame(frame, frame_i)
+	-- Create image/frame context
+	local frame_ctx = g2d.create_context(frame)
+	-- Draw logo onto frame
+	frame_ctx:set_source(g2d.create_source_image(logo_image))
+	frame_ctx:paint()
+end
