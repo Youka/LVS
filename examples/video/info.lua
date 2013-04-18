2ad35c889ebb9856206f84c297d40b5ed0732f7d
Youka <spanknebel.borken@t-online.de>
2013-04-18 18:17:13 +0200

    updated examples

--- examples/video/info.lua
+++ examples/video/info.lua
@@ -1,6 +1,6 @@
 -- Build information string
-local info = string.format("Dimension: %dx%d\nColorspace: %s\nFrames per second: %f\nFrame: %%d/%d"
-							, VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_HAS_ALPHA and "RGBA" or "RGB", VIDEO_FPS, VIDEO_FRAMES)
+local info = string.format("こんにちは\nGuten Tag!\nGood day!\n%s\n\nDimension: %dx%d\nColorspace: %s\nFrames per second: %f\nFrame: %%d/%d"
+							, USERDATA or "", VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_HAS_ALPHA and "RGBA" or "RGB", VIDEO_FPS, VIDEO_FRAMES)
 -- Create position matrix
 local translation = g2d.create_matrix():translate(10,10)
 
@@ -10,7 +10,7 @@ function GetFrame(frame, frame_i)
 	local ctx = g2d.create_context(frame)
 	-- Define path
 	ctx:set_matrix(translation)
-	ctx:path_add_text("こんにちは\nGuten Tag!\nGood day!\n\n" .. info:format(frame_i+1), "Arial", 20)
+	ctx:path_add_text(info:format(frame_i+1), "Arial", 20)
 	-- Stroke path
 	ctx:set_line_width(6)
 	ctx:set_line_join("ROUND")
---
+++ examples/video/stroke.lua
@@ -0,0 +1,33 @@
+-- Defines, strokes and clears lines path
+local function draw_lines(ctx, off_x, off_y)
+	ctx:path_move_to(off_x, off_y)
+	ctx:path_line_to(off_x+100, off_y+100)
+	ctx:path_line_to(off_x+200, off_y)
+	ctx:path_stroke()
+	ctx:path_clear()
+end
+
+-- Process frames
+function GetFrame(frame, frame_i)
+	-- Create image/frame context
+	local ctx = g2d.create_context(frame)
+	-- Set lines color
+	ctx:set_source(g2du.white)
+	-- Set lines width
+	ctx:set_line_width(10)
+	-- Draw lines with round edges
+	ctx:set_line_cap("ROUND")
+	ctx:set_line_join("ROUND")
+	draw_lines(ctx, 10, 10)
+	-- Draw lines with sharp edges
+	ctx:set_line_cap("SQUARE")
+	ctx:set_line_join("MITER")
+	draw_lines(ctx, 10, 40)	
+	-- Draw lines with flat edges
+	ctx:set_line_cap("FLAT")
+	ctx:set_line_join("BEVEL")
+	draw_lines(ctx, 10, 70)
+	-- Draw lines with dashes
+	ctx:set_dash(0, {10, 2, 5})
+	draw_lines(ctx, 10, 100)		
+end
